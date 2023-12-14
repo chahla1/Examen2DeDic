99867e9ff1559d7a8e563b5e81e1ab89bbbdbc4c
chahla1 <cbouchot@myuax.com>
2023-12-14 12:53:58 +0100

    otro commit

--- main.cpp
+++ main.cpp
@@ -11,4 +11,31 @@ struct Asistencia {
     std::string materia;
     std::string estado;  // "Asiste", "Falta", "Tarde"
 };
+class Estudiante {
+public:
+    std::string nombre;
+    int edad;
+    float promedio;
+    std::vector<Materia> materias;
+    std::vector<Asistencia> asistencias;
 
+    void mostrarEstudiante() const {
+        std::cout << "Nombre: " << nombre << "\nEdad: " << edad << "\nPromedio: " << promedio << "\n";
+
+        std::cout << "Materias:\n";
+        for (const auto& materia : materias) {
+            std::cout << "  - " << materia.nombre << ": " << materia.calificacion << "\n";
+        }
+        std::cout << "Asistencias:\n";
+        for (const auto& asistencia : asistencias) {
+            std::cout << "  - Fecha: " << asistencia.fecha << ", Materia: " << asistencia.materia
+                      << ", Estado: " << asistencia.estado << "\n";
+        }
+    }
+
+
+
+    int main(){
+
+    }
+}
